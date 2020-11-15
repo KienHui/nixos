c210c8d14d217964add6ab5c83b344e5f2beb337
KienHui <KienHui@users.noreply.github.com>
2020-11-14 23:53:58 -0600

    Add tailscale

--- configuration.nix
+++ configuration.nix
@@ -71,6 +71,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     git
+    tailscale
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -84,7 +85,9 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  # services.openssh.enable = true;  
+  
+  services.tailscale.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
